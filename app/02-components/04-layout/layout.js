4e9ddf4f8d72fd8dda5893b722121a1ec24d9995
Lauris Mikals <lauris.mikals@gmail.com>
2017-04-16 19:48:15 +0300

    Structuring content

--- app/02-components/04-layout/layout.js
+++ app/02-components/04-layout/layout.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {
   HashRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
+
+import PageLake from '../05-pages/lake'
+import PageContacts from '../05-pages/contacts'
 
 const Home = () => (
   <div>
@@ -17,7 +20,7 @@ const About = () => (
   </div>
 )
 
-export default class Layout extends React.Component {
+export default class Layout extends Component {
   constructor(props) {
     super(props)
   }
@@ -35,9 +38,17 @@ export default class Layout extends React.Component {
             <Link to="about">
               About
             </Link>
+            <Link to="lake">
+              Ezers
+            </Link>
+            <Link to="contacts">
+              Kontakti
+            </Link>
           </header>
           <Route exact path="/" component={Home}/>
           <Route path="/about" component={About}/>
+          <Route path="/lake" component={PageLake}/>
+          <Route path="/contacts" component={PageContacts}/>
         </main>
       </div>
       </Router>
